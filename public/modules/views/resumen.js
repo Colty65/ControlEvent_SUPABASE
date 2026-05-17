4badee847b003a022d18d4087e0b38bcc8c89345
Colty65 <jalvas65@gmail.com>
2026-05-17 21:10:19 +0200

    Add files via upload

--- public/modules/views/resumen.js
+++ public/modules/views/resumen.js
@@ -4,4 +4,7 @@ export function mount({root} = {}){
   if(window.__ceV251 && typeof window.__ceV251.applyZoomColors === 'function'){
     window.__ceV251.applyZoomColors();
   }
+  if(window.__ceV252 && typeof window.__ceV252.apply === 'function'){
+    window.__ceV252.apply();
+  }
 }
