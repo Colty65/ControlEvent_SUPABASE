1a45ae7141d8a2293da29d90059b2e6d7cc3e0fc
Colty65 <jalvas65@gmail.com>
2026-05-17 20:41:47 +0200

    Add files via upload

--- public/modules/views/graficas.js
+++ public/modules/views/graficas.js
@@ -1,3 +1,4 @@
 export function mount({root} = {}){
   if(root) root.dataset.ceModule = 'graficas';
+  if(typeof window.renderGraficas === 'function') window.renderGraficas();
 }
--- public/modules/views/resumen.js
+++ public/modules/views/resumen.js
@@ -1,3 +1,7 @@
 export function mount({root} = {}){
   if(root) root.dataset.ceModule = 'resumen';
+  if(typeof window.renderBudget === 'function') window.renderBudget();
+  if(window.__ceV251 && typeof window.__ceV251.applyZoomColors === 'function'){
+    window.__ceV251.applyZoomColors();
+  }
 }
