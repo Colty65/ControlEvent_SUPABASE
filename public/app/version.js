582f8dcf571f44f702203539ec7b251345dd0ddb
Colty65 <jalvas65@gmail.com>
2026-05-21 01:09:51 +0200

    Add files via upload

--- public/app/version.js
+++ public/app/version.js
@@ -1,3 +1,3 @@
-export const VERSION = 'ControlEvent v30.9.2';
-export const VERSION_FILE = 'ControlEvent_v30_9_2';
-export const PACKAGE_NAME = 'controlevent-v30-9-2-supabase-localhost';
+export const VERSION = 'ControlEvent v30.9.3';
+export const VERSION_FILE = 'ControlEvent_v30_9_3';
+export const PACKAGE_NAME = 'controlevent-v30-9-3-supabase-localhost';
