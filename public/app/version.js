e9d7d3c988e6793ab9b2afdd8c72c79034ea388f
Colty65 <jalvas65@gmail.com>
2026-05-28 00:37:55 +0200

    v50_6;mas mierdas

--- public/app/version.js
+++ public/app/version.js
@@ -1,2 +1,2 @@
-export const VERSION = 'ControlEvent v50.5';
-export const VERSION_FILE = 'ControlEvent_v50_5';
+export const VERSION = 'ControlEvent v50.6';
+export const VERSION_FILE = 'ControlEvent_v50_6';
