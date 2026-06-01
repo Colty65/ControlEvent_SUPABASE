bb9489e4dfc5ccef8d33dcda6a9e4af9b8255a79
Colty65 <jalvas65@gmail.com>
2026-06-01 11:57:47 +0200

    v4_0_1_prod

--- public/app/version.js
+++ public/app/version.js
@@ -1,4 +1,4 @@
-export const VERSION = 'ControlEvent v4.0_prod';
-export const VERSION_FILE = 'ControlEvent_v4_0_prod';
+export const VERSION = 'ControlEvent v4.0.1_prod';
+export const VERSION_FILE = 'ControlEvent_v4_0_1_prod';
 
 window.ControlEventVersion = {version: VERSION, versionFile: VERSION_FILE};
